0aa87242850e7fa57e0b0e578a03237b38825357
Grayson <i@grayson.top>
2022-01-09 14:24:54 +0800

    Auto updated on 2022-01-09 14:24:50.

--- shell/start_notebook.sh
+++ shell/start_notebook.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-BASE_PATH=/mnt/hgfs/Personal/Server/projects/MrDoc
-python3.9 ${BASE_PATH}/manage.py runserver 0.0.0.0:9200
+BASE_PATH=/usr/local/projects/MrDoc
+python3 ${BASE_PATH}/manage.py runserver 0.0.0.0:9200
