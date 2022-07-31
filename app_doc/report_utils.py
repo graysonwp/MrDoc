0e90fb20c44ae9ddbfeccec6b54db5a5f330cfce
Grayson <i@grayson.top>
2022-07-31 14:03:57 +0000

    modified:   app_doc/report_utils.py

--- app_doc/report_utils.py
+++ app_doc/report_utils.py
@@ -163,7 +163,7 @@ class ReportMD():
         md_file = shutil.make_archive(
             base_name=self.report_file_path,
             format='zip',
-            root_dir='{}'.format(self.project_path)
+            root_dir=os.path.dirname(self.project_path)
         )
         # 删除文件夹
         shutil.rmtree(self.project_path)
